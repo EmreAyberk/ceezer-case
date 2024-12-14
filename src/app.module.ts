5e5e5a675fb3cf9b971fcf045c0da8340d4ea019
Emre Ayberk Kaymaz <emreayberk23@gmail.com>
2024-12-14 22:07:14 +0300

    feat: CZR-01 portfolio table created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PortfolioController } from './portfolio/portfolio.controller';
+import { PortfolioService } from './portfolio/portfolio.service';
+import { PortfolioModule } from './portfolio/portfolio.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppConfig, DatabaseConfig } from './config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [AppConfig, DatabaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+      inject: [ConfigService],
+    }),
+    PortfolioModule],
+  controllers: [AppController, PortfolioController],
+  providers: [AppService, PortfolioService],
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ src/entities/portfolio.entity.ts
@@ -0,0 +1,34 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('portfolio')
+export class Portfolio {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  country: string;
+
+  @Column()
+  image: string;
+
+  @Column({ name: 'price_per_ton' })
+  pricePerTon: number;
+
+  @Column({ name: 'offered_value_in_tons' })
+  offeredVolumeInTons: number;
+
+  @Column({ type: 'float', name: 'distribution_weight' })
+  distributionWeight: number;
+
+  @Column({ name: 'supplier_name' })
+  supplierName: string;
+
+  @Column({ type: 'date', name: 'earliest_delivery' })
+  earliestDelivery: Date;
+
+  @Column()
+  description: string;
+}
